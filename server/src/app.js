8351842406f9ceca25aa399ef101ad2aff02b8c7
Darshan <darshangowda99001234@gmail.com>
2026-07-25 19:09:00 +0530

    Added Swagger API documentation

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import { load as yamlLoad } from "js-yaml";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -28,6 +33,19 @@ app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const openApiPath = path.resolve(__dirname, "../../docs/openapi.yaml");
+const openApiDocument = yamlLoad(
+  fs.readFileSync(openApiPath, "utf8")
+);
+
+app.use(
+  "/api/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(openApiDocument)
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
